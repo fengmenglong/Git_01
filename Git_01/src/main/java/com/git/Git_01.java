2d1acb4e8f21dd7677689c4fbae7978aa5a5ef1e
Eric <634904037@qq.com>
2021-07-04 21:55:02 +0800

    创建了dev分支

--- Git_01/src/main/java/com/git/Git_01.java
+++ Git_01/src/main/java/com/git/Git_01.java
@@ -8,5 +8,6 @@ public class Git_01 {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("更新1");
+        System.out.println("已创建dev分支");
     }
 }
