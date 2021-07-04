fb601a00b9bce3ea007ab58ece12055d5eeb3d99
Eric <634904037@qq.com>
2021-07-04 21:38:20 +0800

    提交第一次

---
+++ Git_01/src/main/java/com/git/Git_01.java
@@ -0,0 +1,11 @@
+/*
+@author shkstart
+@create 2021-07-04-21:29
+*/
+package com.git;
+
+public class Git_01 {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
